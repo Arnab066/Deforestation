2c0ddc875b3ce5497d548c7838f4ca52e1cdc86f
Arnab Chowdhury <chowdhuryarnab066@gmail.com>
2026-08-24 23:07:17 +0530

    feat: configure Vercel serverless deployment rewrite to api/index.py

---
+++ api/index.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+# Add root directory to python path
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from app import app
+
+# Export WSGI application entrypoint for Vercel
+handler = app
